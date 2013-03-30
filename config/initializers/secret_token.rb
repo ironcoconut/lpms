0b8b1f2a7f021b399835387354e0e52a75299a82
ironcoconut <ironcoconut@gmail.com>
2013-03-29 22:41:57 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lpms::Application.config.secret_token = '669d665faea7ba088f2984d291108436fcc2c34db9de75dadc58f5602d50c4f52728096bc56f4a036ddba2935da19335bdec510582c4eedbe11d752a71122229'
